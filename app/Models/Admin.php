688195987a9037313f30ce30cb0e465c5e015d7e
golpin <ttmessage1976@gmail.com>
2021-07-23 20:09:39 +0900

    user/admin/post table,model.controller

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,17 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Admin extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    protected $table = 'users';
-
+    protected $table = 'admins';
+    
     protected $fillable = [
         'name',
         'email',
@@ -37,9 +35,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function post()
-    {
-        return $this->hasMany(Post::class);
-    }
 }
