92b2ce85fa4f13853425402469b5aa679252739d
golpin <ttmessage1976@gmail.com>
2021-07-24 20:27:52 +0900

    ゲスト/ユーザーのルート、コントローラー、ビュー

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+
+Route::get('/', [PostController::class, 'guest'])->name('guest');
+
+Route::middleware('auth:users')->group(function () {
+    Route::get('/home', [PostController::class, 'home'])->name('home');
+    Route::get('/create', [PostController::class, 'create'])->name('create');
+    Route::post('/store', [PostController::class, 'store'])->name('store');
+    Route::post('/delete/{id}', [PostController::class, 'delete'])->name('delete');
+    Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [PostController::class, 'update'])->name('update');
+});
+
+
+require __DIR__ . '/auth.php';
