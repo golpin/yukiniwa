2d98bb7f652c42a14a3d169d9d3de9c30b6d16be
golpin <ttmessage1976@gmail.com>
2021-07-26 22:39:49 +0900

    Profile/modelの設定

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $table = 'profiles';
+
+    protected $fillable =
+    [
+        'content',
+        'icon',
+        'user_id',
+        'ski_resort_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function ski_resort()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
