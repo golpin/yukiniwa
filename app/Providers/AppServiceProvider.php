06f9f525491b8591292f845cacff2bcb088aeed7
golpin <ttmessage1976@gmail.com>
2021-08-09 22:33:09 +0900

    none

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(180);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
