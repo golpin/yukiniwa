d97748fed2368e9ace588aa885fbd59e3ec57ea5
golpin <mokomokogoltusamu@gmail.com>
2021-08-22 20:49:58 +0900

    管理者画面モーダルの調整

--- resources/views/components/sort_ski_resort.blade.php
+++ resources/views/components/sort_ski_resort.blade.php
@@ -1,19 +1,16 @@
 <div class="flex flex-col sm:flex-row">
-    <span class="mx-2 my-auto">
-        スキー場絞り込み
+    <span class="mx-2 my-auto text-md">
+        スキー場ソート
     </span>
     <select name="ski_resort" id="ski_resort" class="text-sm ">
-        <option value="0" @if(\Request::get('ski_resort')=="0" ) selected @endif>
+        <option value="0" @if (\Request::get('ski_resort') == '0') selected @endif>
             全て
         </option>
-        foreach ($ski_resorts as $ski_resort){
-
-            
-        }
-        <option value="{{ $ski_resort->id }}" @if(\Request::get('ski_resort')==$ski_resort->id )
-            selected @endif>
-            {{ $ski_resort->name }}
-        </option>
+        @foreach ($ski_resorts as $ski_resort)
+            <option value="{{ $ski_resort->id }}" @if (\Request::get('ski_resort') == $ski_resort->id) selected @endif>
+                {{ $ski_resort->name }}
+            </option>
         @endforeach
     </select>
 </div>
+
